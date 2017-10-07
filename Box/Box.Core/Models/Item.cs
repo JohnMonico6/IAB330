ee0bffa6f6f69cb9ae731d9240bca37e8514344e
Robert Piper <mrholypally@gmail.com>
2017-10-07 21:54:21 +1000

    Made some changes for review in packages and made the item class

---
+++ Box/Box.Core/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqliteTutorial.Core.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public Item (string Name, int Quantity)
+        {
+            this.Name = Name;
+            this.Quantity = Quantity;
+        }
+    }
+}
--- Box/Box.Core/Models/Package.cs
+++ Box/Box.Core/Models/Package.cs
@@ -7,7 +7,7 @@ using SQLite.Net.Attributes;
 
 namespace SqliteTutorial.Core.Models
 {
-    class Package
+    public class Package
     {
         [PrimaryKey]
         [AutoIncrement]
@@ -15,13 +15,39 @@ namespace SqliteTutorial.Core.Models
         public string Name { get; set; }
         public string Items { get; set; } // Serialized CSV
         public string Room { get; set; }
-        public List<string> GetItemList()
+        public List<Item> GetItemList()
         {
+            List<string> tempList = Items.Split(',').ToList();
+            List<Item> items = new List<Item>();
+            for (int x = 0; x < tempList.Count; x++)
+            {
+                if (x % 2 != 0)
+                {
+                       
+                    items.Add(new Item(tempList[x - 1], Int32.Parse(tempList[x])));
+                }
+            }
             return null; // sup
         }
-        public void SetItemList(List<string> items)
+        public void SetItemList(List<Item> items)
         {
-
+            string temp = "";
+            foreach (Item it in items)
+            {
+                if (temp != "")
+                {
+                    temp += ",";
+                }
+                temp += it.Name;
+                temp += ",";
+                temp += it.Quantity;
+            }
+            Items = temp;
+        }
+        public List<Item> AddItem(List<Item> items, string Name, int Quantity)
+        {
+            items.Add(new Item(Name, Quantity));
+            return items;
         }
     }
 
